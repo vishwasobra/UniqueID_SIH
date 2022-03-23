457bed9269ed72a643848a66ee761443e834b7e5
harshchhabra37 <chhabraharsh37@gmail.com>
2022-03-23 12:11:55 +0530

    upload file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:unique_id_app/login_signup.dart';
+import 'package:unique_id_app/upload.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -27,7 +31,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginSignup(),
+      home: Upload(),
     );
   }
 }
